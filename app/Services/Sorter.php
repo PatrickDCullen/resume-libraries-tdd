15923262dd18b5a51b705037794f58c9e60cd12e
PatrickDCullen <cullenpatrickd@gmail.com>
2026-02-11 00:28:08 -0500

    feat: get NPM requirements by downloads test created and passed, rough draft

--- app/Services/Sorter.php
+++ app/Services/Sorter.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Http\Clients\Composer;
+use Illuminate\Support\Facades\Http;
 
 class Sorter
 {
@@ -17,6 +18,28 @@ class Sorter
     {
         return collect($requirements)->map(function ($package) {
             $downloads = $this->client->getMonthlyDownloads($package);
+
+            return ['package' => $package, 'downloads' => $downloads];
+        })->sortByDesc('downloads')
+            ->values()
+            ->map(function ($item) {
+                return $item['package'];
+            })
+            ->all();
+    }
+
+    public function sortNpmRequirementsByDownloads(array $requirements)
+    {
+        return collect($requirements)->map(function ($package) {
+            $url = 'https://registry.npmjs.org/-/v1/search';
+            $query = ['text' => $package, 'size' => 1];
+            // Does this create an HTTP client each time?
+            // Or is it created already (or once per app life cycle when Http::get() is called?)
+            // TODO see https://medium.com/@harrisrafto/simplifying-api-interactions-with-laravels-http-client-9d1b432035b5
+            // for guzzle options (debug to answer my question)
+            // and macros, which appear to be the equivalent to client like I was using above
+            $response = Http::get($url, $query);
+            $downloads = json_decode($response->getBody()->getContents(), true)['objects'][0]['downloads']['monthly'];
 
             return ['package' => $package, 'downloads' => $downloads];
         })->sortByDesc('downloads')
--- tests/Feature/SorterTest.php
+++ tests/Feature/SorterTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Services\Sorter;
+use Illuminate\Support\Facades\Http;
 
 // This test alone adds 0.5s to suite when "fresh", 0.25 when cached by Packagist
 // (actually half that when client is instantiated outside of loop)
@@ -15,4 +16,31 @@ test('giving the sorter an array of dependencies outputs the array sorted by dow
     // Then
     $expected = ['fakerphp/faker', 'juling/laravel-devtools'];
     expect($composerRequirementsByDownload)->toBe($expected);
+});
+
+// TODO
+// refactor to HTTP facade vs. Guzzle Client?
+// figure out how to run "integration" tests outside? (or maybe, for now just a way to manually run them once when merging)
+
+test('the sorter can take an array of JavaScript dependencies and sort it by downloads, descending', function () {
+    // Given
+    $npmDependencies = ['react-native', 'axios', 'tailwindcss', 'vite'];
+
+    // When
+    Http::preventStrayRequests();
+    Http::fake([
+        // We stub out JSON HTTP response in the shape we expect from NPM Registry endpoints...
+        'https://registry.npmjs.org/*' => Http::sequence()
+            ->push(['objects' => [['downloads' => ['monthly' => 500]]]])
+            ->push(['objects' => [['downloads' => ['monthly' => 499]]]])
+            ->push(['objects' => [['downloads' => ['monthly' => 3]]]])
+            ->push(['objects' => [['downloads' => ['monthly' => 50000]]]]),
+    ]);
+    // And we run the sorter's sortNpmRequirementsByDownloads() method
+    $sortedDependencies = (new Sorter)->sortNpmRequirementsByDownloads($npmDependencies);
+
+    // Then
+    // we expect the sorter array to be in the order that corresponds to
+    // our libraries and the canned responses from the NPM endpoint
+    expect($sortedDependencies)->toBe(['vite', 'react-native', 'axios', 'tailwindcss']);
 });
