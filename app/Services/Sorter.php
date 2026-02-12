a9ec3a60604bd56f97fe5b2d253932b870ece545
PatrickDCullen <cullenpatrickd@gmail.com>
2026-02-12 14:22:56 -0500

    feat: implement caching for Composer and NPM

--- app/Services/Sorter.php
+++ app/Services/Sorter.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Http\Clients\Composer;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class Sorter
@@ -17,7 +18,12 @@ class Sorter
     public function sortComposerRequirementsByDownloads(array $requirements)
     {
         return collect($requirements)->map(function ($package) {
-            $downloads = $this->client->getMonthlyDownloads($package);
+            $downloads = Cache::get($package);
+
+            if (is_null($downloads)) {
+                $downloads = $this->client->getMonthlyDownloads($package);
+                Cache::put($package, $downloads);
+            }
 
             return ['package' => $package, 'downloads' => $downloads];
         })->sortByDesc('downloads')
@@ -33,21 +39,27 @@ class Sorter
         return collect($requirements)->map(function ($package) {
             $url = 'https://registry.npmjs.org/-/v1/search';
             $query = ['text' => $package, 'size' => 1];
-            // (Speed) Does this create an HTTP client each map iteration?
-            // Or is it created already (or once per app life cycle when Http::get() is called?)
-            $response = Http::withOptions([
-                // Keeping for reference, great to have for debugging
-                // 'debug' => true,
-                ['version' => '2.0'],
-            ])->retry(5, function ($try, $response) {
-                // Linear backoff - for each time we try a given request,
-                // add 200ms up to waiting a full second on the fifth and final time
-                // We do this because NPM API is not transparent at all about what they expect
-                // and all retry-after headers are 0 on 429 Too Many Requests responses
-                return $try * 200;
-            })->get($url, $query);
 
-            $downloads = json_decode($response->getBody()->getContents(), true)['objects'][0]['downloads']['monthly'];
+            $downloads = Cache::get('npm'.$package);
+
+            if (is_null($downloads)) {
+                // (Speed) Does this create an HTTP client each map iteration?
+                // Or is it created already (or once per app life cycle when Http::get() is called?)
+                $response = Http::withOptions([
+                    // Keeping for reference, great to have for debugging
+                    // 'debug' => true,
+                    ['version' => '2.0'],
+                ])->retry(5, function ($try, $response) {
+                    // Linear backoff - for each time we try a given request,
+                    // add 200ms up to waiting a full second on the fifth and final time
+                    // We do this because NPM API is not transparent at all about what they expect
+                    // and all retry-after headers are 0 on 429 Too Many Requests responses
+                    return $try * 200;
+                })->get($url, $query);
+
+                $downloads = json_decode($response->getBody()->getContents(), true)['objects'][0]['downloads']['monthly'];
+                Cache::put('npm'.$package, $downloads);
+            }
 
             return ['package' => $package, 'downloads' => $downloads];
         })->sortByDesc('downloads')
